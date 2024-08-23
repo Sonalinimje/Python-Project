7703d8d5fec0fd77dcc8aa85cff38c7f764e0829
Sonali Nimje <nimje.sweety26@gmail.com>
2024-08-23 21:56:07 +0530

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,8 @@
+# open contact.py
+class Contact:
+    def __init__(self,name,phone,email):
+        self.name=name
+        self.phone=phone
+        self.email=email
+    def display_contact(self):
+        print(f"Name:{self.name}\nPhone:{self.phone}\nEmail:{self.email}")    
---
+++ contact_book.sql
@@ -0,0 +1,8 @@
+create database contact_book;
+use contact_book;
+create table contacts(
+	 id int auto_increment primary key,
+     name varchar(100) not null,
+     phone varchar(20),
+     email varchar(100)
+);
